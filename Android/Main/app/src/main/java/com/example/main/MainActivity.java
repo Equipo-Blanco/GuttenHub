a3b08e96d246449680eb328dcf4a4a0e72254b9e
kimikazio <sergioluhia@gmail.com>
2020-12-13 22:29:57 +0100

    intent partners

--- Android/Main/app/src/main/java/com/example/main/MainActivity.java
+++ Android/Main/app/src/main/java/com/example/main/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bot_partners;
+    Button bot_citas;
+    Button bot_pedidos;
+    Button bot_envios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bot_partners = (Button) findViewById(R.id.BtnPartners);
+
+        Intent intentPartner = new  Intent(this, partners.class);
+
+        bot_partners.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intentPartner);
+            }
+        });
     }
 }
