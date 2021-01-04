ac0a5f1b5bc7d88853392a54032bd689b122bb46
kimikazio <56109205+kimikazio@users.noreply.github.com>
2021-01-04 20:23:43 +0100

    ahora actualiza lista de xml

--- Android/Main/app/src/main/java/com/example/main/Pedidos.java
+++ Android/Main/app/src/main/java/com/example/main/Pedidos.java
@@ -23,7 +23,7 @@ public class Pedidos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedidos);
-        Log.d(TAG, "OnCreate: Starting.");
+        Log.d(TAG, "creado");
         bot_catalogo = (Button) findViewById(R.id.btn_catalogo);
 
         Intent intento = new Intent(this, Catalogo.class);
--- Android/Main/app/src/main/java/com/example/main/Tab2_pedidos_hechos.java
+++ Android/Main/app/src/main/java/com/example/main/Tab2_pedidos_hechos.java
@@ -1,11 +1,11 @@
 package com.example.main;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.LayoutInflater;
@@ -18,25 +18,27 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 public class Tab2_pedidos_hechos extends Fragment {
     private static final String TAG = "Tab2Fragment";
     ListView listaPresupuestos;
+    Button bot_actualizar;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab2_pedidos_hechos, container, false);
         listaPresupuestos = view.findViewById(R.id.lv_presupuestos);
+        bot_actualizar = view.findViewById(R.id.btn_actualizar);
+
 
         File rutaPresupuestos = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/DraftPresupuestos");
-        String archivos[] = rutaPresupuestos.list();
-        System.out.println("List of files and directories in the specified directory:");
-        for (int i = 0; i < archivos.length; i++) {
-            System.out.println(archivos[i]);
-        }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_expandable_list_item_1, archivos);
+        List<String> archiv;
+        archiv = Arrays.asList(rutaPresupuestos.list());
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_expandable_list_item_1, archiv);
         listaPresupuestos.setAdapter(adapter);
 
         Intent intentPresup = new Intent(getContext(), DetallePresupuesto.class);
@@ -44,13 +46,20 @@ public class Tab2_pedidos_hechos extends Fragment {
         listaPresupuestos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String elegido = archivos[i];
+                String elegido = archiv.get(i);
                 intentPresup.putExtra("nombrexml", elegido);
                 startActivity(intentPresup);
 
             }
         });
 
+        bot_actualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+                startActivity(getActivity().getIntent());
+            }
+        });
         return view;
     }
 }
